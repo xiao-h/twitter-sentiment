71ab857d21f56d958a96942b1dbaa868f951dba4
xiao-h <hsiao.xiaoxiao@gmail.com>
2017-06-21 15:57:44 -0400

    data preprocessing uploaded

---
+++ preprocessing/tweets_preprocess.py
@@ -0,0 +1,48 @@
+import re
+
+#Processing Tweets
+
+def preprocessTweets(tweet):
+
+    #Convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+
+    #Convert @username to __HANDLE
+    tweet = re.sub('@[^\s]+','__HANDLE',tweet)
+
+    #Replace #word with word
+    tweet = re.sub(r'#([^\s]+)', r' \1', tweet)
+
+    #trim
+    tweet = tweet.strip('\'"')
+
+    # Repeating words like happyyyyyyyy
+    rpt_regex = re.compile(r"(.)\1{1,}", re.IGNORECASE)
+    tweet = rpt_regex.sub(r"\1\1", tweet)
+
+    #Emoticons
+    emoticons = \
+    [
+     ('positive',[ ':-)', ':)', '(:', '(-:', \
+                       ':-D', ':D', 'X-D', 'XD', 'xD', \
+                       '<3', ':\*', ';-)', ';)', ';-D', ';D', '(;', '(-;', ] ),\
+     ('negative', [':-(', ':(', '(:', '(-:', ':,(',\
+                       ':\'(', ':"(', ':((', ] ),\
+    ]
+
+    def replace_parenth(arr):
+       return [text.replace(')', '[)}\]]').replace('(', '[({\[]') for text in arr]
+
+    def regex_join(arr):
+        return '(' + '|'.join( arr ) + ')'
+
+    emoticons_regex = [ (repl, re.compile(regex_join(replace_parenth(regx))) ) \
+            for (repl, regx) in emoticons ]
+
+    for (repl, regx) in emoticons_regex :
+        tweet = re.sub(regx, ' '+repl+' ', tweet)
+
+     #Convert to lower case
+    tweet = tweet.lower()
+
+    return tweet
